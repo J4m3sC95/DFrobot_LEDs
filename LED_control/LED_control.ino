8d697b39bfd4c826714690bc8bed1a534d3815fb
James <jcoulson95@gmail.com>
2017-05-29 20:59:56 +0100

    removed commented crap and added description

--- LED_control/LED_control.ino
+++ LED_control/LED_control.ino
@@ -1,7 +1,13 @@
 /*
- * DFrobot Beetle LED control sketch
+ * DFrobot Beetle LED control sketch (based upon the iron man control sketch)
  * 
- * (based upon the iron man control sketch)
+ * Effects:
+ * - All leds on
+ * - All leds flashing
+ * - leds individually lighting in a circle
+ * - individual random leds lighting
+ * - trippy pair flash
+ * - just blue light for torch (blue is very bright)
  * 
  */
 
@@ -22,8 +28,6 @@ uint8_t pins[5] = {
 #define RANDOM_FLASH                    4
 #define PAIR_FLASH                      5
 #define BLUE                            6
-
-
 
 int delay_array [EFFECT_COUNT + 1] = {0, 0, 150, 100, 100, 1000};
 
@@ -47,8 +51,6 @@ int effect_delay = 1000;
 unsigned long effect_timer = 0;
 
 void setup() {
-  //Serial.begin(115200);
-  
   pinMode(BUTTON_PIN, INPUT_PULLUP);
   pinMode(3, OUTPUT);
   digitalWrite(3, LOW);
@@ -56,9 +58,6 @@ void setup() {
   for(int n = 0; n < 4; n++){
     pinMode(pins[n], OUTPUT);
   }
-
-  
-
 }
 
 void loop() {
@@ -238,119 +237,3 @@ void randomLed(void){
   }
   prev_random = new_random;
 }
-
-/*
-void allFlash(void){
-  int flash_state;
-  if(count == 0){
-    flash_state = HIGH;
-  }
-  else if(count == 1){
-    flash_state = LOW;
-  }
-  for(int n = 0; n < 4; n++){
-    digitalWrite(pins[n], flash_state);
-  }
-  count++;
-  if(count == 2){
-    count = 0;
-  }
-}
-*/
-
-
-
-
-
-/*
-
-void allLedsOff(void){
-  for(int n = 0; n<24; n++){
-    strip.setPixelColor(n, off);
-  }
-  strip.show();
-}
-
-void allLedsOn(uint32_t colour){
-    for(int n = 0; n<24; n++){
-      strip.setPixelColor(n, colour);
-    }
-    strip.show();
-}
-
-void randomRingFlash(void){
-  if(count == 0){
-    ringColour = Wheel(random(255));
-    allLedsOn(ringColour);
-  }
-  else if(count == 2){
-    allLedsOff();
-  }
-
-  count++;
-  if(count == 3){
-    count = 0;
-  }
-  //delay(100);
-}
-
-void randomPixelRingFlash(void){
-  if(count == 0){
-    for(int n = 0; n<24; n++){
-      strip.setPixelColor(n, Wheel(random(255)));
-    }
-    strip.show();
-  }
-  else if(count == 2){
-    allLedsOff();
-  }
-
-  count++;
-  if(count == 3){
-    count = 0;
-  }
-
-  //delay(100);
-}
-
-void loading(void){
-  for(int n = 0; n<24; n++){
-    position_lag = n-count;
-    position_lag1 = (n-24)-count;
-    if(abs(position_lag1) < abs(position_lag)){
-      position_lag = position_lag1;      
-    }
-    if((position_lag > -4) && (position_lag <= 0)){
-      strip.setPixelColor(n,255/(BRIGHTNESS_FACTOR*position_lag),255/(BRIGHTNESS_FACTOR*position_lag),255/(BRIGHTNESS_FACTOR*position_lag));
-    }
-    else{
-      strip.setPixelColor(n, 0,0,0);
-    }
-  }
-  strip.show();
-  count++;
-  if(count == 24){
-    count = 0;
-  }
-  //delay(100);
-}
-
-void rainbow_wheel(){
-  for(int n=0; n<24; n++){
-    colour_index = count + (n*10);
-    strip.setPixelColor(n, Wheel(colour_index));
-  }
-  strip.show();
-  count++;
-  //delay(10);
-}
-
-void rainbow(){
-  for(int m = 0; m< 24; m++){
-    strip.setPixelColor(m, Wheel(count));
-  }
-  strip.show();
-  count++;
-  //delay(10);
-}
-*/
